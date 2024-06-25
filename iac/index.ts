6f4b88619d003def1178ebb181455c260a33fbd0
ychen44 <ya4chen@gmail.com>
2024-06-24 17:19:34 -0700

    add vpc dir

--- iac/index.ts
+++ iac/index.ts
@@ -1,9 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { vpcId } from "./vpc";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+export const myVpcId = vpcId;
+
+// // Create an AWS resource (S3 Bucket)
+// const bucket = new aws.s3.Bucket("my-bucket");
+
+// // Export the name of the bucket
+// export const bucketName = bucket.id;
--- iac/vpc/index.ts
+++ iac/vpc/index.ts
@@ -1,14 +1,31 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+import * as path from 'path';
 
-// Create a new VPC
-const vpc = new aws.ec2.Vpc("my-vpc", {
-    cidrBlock: "10.0.0.0/16",
-    enableDnsHostnames: true,
-    enableDnsSupport: true,
+
+interface VpcConfig {
+    cidrBlock: string;
+    enableDnsHostnames: boolean;
+    enableDnsSupport: boolean;
     tags: {
-        Name: "my-vpc",
-    },
+        Name: string;
+    };
+}
+
+
+const configPath = path.join(__dirname, 'config.yaml');
+
+
+const config: VpcConfig = yaml.load(fs.readFileSync(configPath, 'utf8')) as VpcConfig;
+
+// Create a new VPC using the configurations from the YAML file
+const vpc = new aws.ec2.Vpc("my-vpc", {
+    cidrBlock: config.cidrBlock,
+    enableDnsHostnames: config.enableDnsHostnames,
+    enableDnsSupport: config.enableDnsSupport,
+    tags: config.tags,
 });
 
 // Export the VPC ID
